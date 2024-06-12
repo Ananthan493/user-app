8691ecb1b71a6ec7c03e10c4deaa571e2e6cb665
Ananthan493 <ananthanofficial493@gmail.com>
2024-06-13 00:34:42 +0530

    add user updated

--- src/components/Adduser.jsx
+++ src/components/Adduser.jsx
@@ -130,7 +130,7 @@ const Adduser = () => {
                             <input type="text" className="input form-control" />
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <button className="btn btn-success"></button>
+                            <button className="btn btn-success">Register</button>
                         </div>
                     </div>
                 </div>
